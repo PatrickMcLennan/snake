7e2873e5703f10684e81299833f18b55ded4f4fb
Patrick <patrick.a.mclennan@gmail.com>
2018-10-04 16:12:15 -0400

    working on Snake class

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
+const food = document.querySelector('.food');
+
 class Snake {
     constructor() {
         this.head = document.querySelector('.head');
         this.body = [this.head];
-        // this.direction = this.getDirection();
+        this.direction = this.getDirection();
+        this.x = 10;
+        this.y = 10;
     }
     move() {
-        console.log(this.body);
-
+        setTimeout( () => {
+            if (this.direction === 38) {
+                this.x--;
+            } else if (this.direction === 39) {
+                this.y++;
+            } else if (this.direction === 40) {
+                this.x++;
+            } else if (this.direction === 37){
+                this.y--;
+            }
+            this.head.style.gridArea = `${this.x} / ${this.y} / ${this.x + 1} / ${this.y + 1}`;
+            this.move();
+        }, 750)
     }
     getDirection() {
         setTimeout( () => {
-            let direction;
-            window.addEventListener('keydown', e => e.keyCode === direction);
+            window.addEventListener('keydown', e => this.direction = e.keyCode);
             this.getDirection();
-            console.log(direction);
-            return direction;
-        }, 1000)
+        }, 750)
+    }
+    grow() {
+        console.log('yum');
+        setFood();
     }
 }
-
+function setFood() {
+    const [x, y] = [(Math.floor(Math.random() * 20)), (Math.floor(Math.random() * 20))];
+    food.style.gridArea = `${x} / ${y} / ${x + 1} / ${y + 1}`;
+}
 
 const snake = new Snake;
-snake.move();
-console.log(snake.direction);
+snake.move()
+setFood();
+if (snake.head.style.gridArea === food.style.gridArea) {
+    snake.grow();
+}
