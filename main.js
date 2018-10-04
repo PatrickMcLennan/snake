cacc696c58c5f2fe1cd582e05caf7fcaca849e62
Patrick <patrick.a.mclennan@gmail.com>
2018-10-04 14:48:11 -0400

    working on Snake class

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-window.addEventListener('click', () => console.log('hello world!'));
+class Snake {
+    constructor() {
+        this.body = [];
+        this.body[0] = document.querySelector('.head');
+        this.direction = window.addEventListener('keydown', e => direction == e.keyCode);
+    }
+    move() {
+        console.log(this.body);
+
+    }
+    getDirection() {
+        setTimeout( () => {
+            console.log(this.direction);
+            this.getDirection();
+        }, 1000)
+    }
+}
+
+
+const snake = new Snake;
+snake.move();
+snake.getDirection();
