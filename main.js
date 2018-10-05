e4aed348d381b3164d5c2c20c10d2e76cc611cb3
Patrick <patrick.a.mclennan@gmail.com>
2018-10-04 22:16:47 -0400

    tail is driving me nuts

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const canvas = document.querySelector('.canvas');
 
 class Snake {
     constructor() {
-        this.head = document.querySelector('.canvas div');
-        this.body = [this.head];
+        this.body = [1];
+        this.body[0] = document.createElement('div');
         this.direction = window.addEventListener('keydown', e => this.direction = e.keyCode);
         [this.x, this.y] = [10, 10];
         this.speed = 150;
@@ -20,25 +20,13 @@ class Snake {
             } else if (this.direction === 37){
                 this.y--;
             } 
-            this.head.style.gridArea = `${this.x} / ${this.y} / ${this.x + 1} / ${this.y + 1}`;
-            if(this.head.style.gridArea === food.style.gridArea) {
+            this.body[0].style.gridArea = `${this.x} / ${this.y} / ${this.x + 1} / ${this.y + 1}`;
+            if(this.body[0].style.gridArea === food.style.gridArea) {
                 this.grow();
                 newFood();
             }
-            if (this.body.length >= 2) {
-                this.tailManagement(this.body);
-            }
             this.move();
         }, this.speed);
-    }
-    grow() {
-        const limb = document.createElement('div');
-        canvas.appendChild(limb);
-        this.body.unshift(limb);
-        console.log(this.body);
-    }
-    tailManagement(array) {
-        array.forEach()
     }
 }
 
@@ -46,9 +34,11 @@ function newFood() {
     const [x, y] = [(Math.floor(Math.random() * 20)), (Math.floor(Math.random() * 20))];
     food.style.gridArea = `${x} / ${y} / ${x + 1} / ${y + 1}`;
 }
-function start() {
+function play() {
     const snake = new Snake;
-    snake.move()
+    canvas.appendChild(snake.body[0]);
     newFood();
+    snake.move();
 }
-start();
+
+play();
