bf05183664f3aa7f43a70b76cdd0e72cff3680cd
Patrick <patrick.a.mclennan@gmail.com>
2018-10-04 21:54:18 -0400

    tail is driving me nuts

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const canvas = document.querySelector('.canvas');
 
 class Snake {
     constructor() {
-        this.body = [document.querySelector('.body')];
-        this.head = this.body[0];
-        this.direction = this.getDirection();
+        this.head = document.querySelector('.canvas div');
+        this.body = [this.head];
+        this.direction = window.addEventListener('keydown', e => this.direction = e.keyCode);
         [this.x, this.y] = [10, 10];
         this.speed = 150;
     }
     move() {
-        setTimeout( () => {
+        setTimeout(() => {
             if (this.direction === 38) {
                 this.x--;
             } else if (this.direction === 39) {
@@ -21,29 +21,24 @@ class Snake {
                 this.y--;
             } 
             this.head.style.gridArea = `${this.x} / ${this.y} / ${this.x + 1} / ${this.y + 1}`;
-
-            if (this.head.style.gridArea === food.style.gridArea) {
+            if(this.head.style.gridArea === food.style.gridArea) {
                 this.grow();
                 newFood();
+            }
+            if (this.body.length >= 2) {
+                this.tailManagement(this.body);
             }
             this.move();
         }, this.speed);
     }
-    getDirection() {
-        setTimeout(() => {
-            window.addEventListener('keydown', e => this.direction = e.keyCode);
-            this.getDirection();
-        }, this.speed);
-    }
     grow() {
-        const addition = document.createElement('div');
-        addition.classList.add('body');
-        canvas.appendChild(addition)
-        this.body.push(addition);
+        const limb = document.createElement('div');
+        canvas.appendChild(limb);
+        this.body.unshift(limb);
         console.log(this.body);
     }
-    tail() {
-        this.body.shift();
+    tailManagement(array) {
+        array.forEach()
     }
 }
 
@@ -55,7 +50,5 @@ function start() {
     const snake = new Snake;
     snake.move()
     newFood();
-    
 }
-
 start();
